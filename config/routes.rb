e9060eedf8fcfe1e5ffef9d46f93b0db272e890f
Daniyar Bazakov <bazakovd@gmail.com>
2019-11-28 10:12:45 -0500

    added btns

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ resources :consumption, only: [:new, :create, :show, :index, :update]
 
   devise_for :users
   root to: 'pages#home'
+
+  get "/style", to: "pages#style"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
