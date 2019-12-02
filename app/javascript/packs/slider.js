ea7fe9251852069b5a27a299c1a129c6ed6578a0
bnadal <benjamin-nadal@hotmail.fr>
2019-12-02 12:19:14 -0500

    bencommit

--- app/javascript/packs/slider.js
+++ app/javascript/packs/slider.js
@@ -7,3 +7,42 @@ $('.horrible-fact-slider').slick(
   {
     arrows: false
   });
+
+
+$('.horrible-fact-slider').on('swipe', function(event, slick, direction){
+  console.log(direction);
+  console.log(slick);
+ 
+  // select all with class .random-card
+   var x = document.querySelectorAll(".random-card")
+
+  // check their parents
+  let activeElement = null;
+
+  x.forEach((el) => {
+    if (el.parentElement.parentElement.getAttribute("aria-hidden") === "false") {
+      // when we find a parent with attribute aria-hidden=false => that's our active slide
+      // reselect le children pour extract le id de la carte
+
+      activeElement = el;
+    };
+  });
+
+  // extraire le fact de la carte id-fact
+  let idFact = activeElement.querySelector(".card-fact p").innerText
+  console.log(idFact)
+  // extraire la photo de la carte id-photo
+  let idPhoto = activeElement.querySelector(".card-photo img").getAttribute("src")
+  console.log(idPhoto)
+
+  // remove le meta tag de og-description
+  $('meta[property="og:description"]').remove();
+  $('head').append( `<meta property="og:description" content="${idFact}">` );
+
+  $('meta[property="og:image"]').remove();
+  $('head').append( `<meta property="og:image" content="${idPhoto}">` );
+    // ajouter le meta tag de description
+  // remove le meta tag de og-image 
+    // ajouter le meta tag de image
+
+});
