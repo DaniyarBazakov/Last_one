a332d90b70673a9fa79449bb3171d9844f348af7
bnadal <benjamin-nadal@hotmail.fr>
2019-12-04 14:32:22 -0500

    add404and500pages

--- app/javascript/packs/wallet.js
+++ app/javascript/packs/wallet.js
@@ -3,17 +3,16 @@ const deposit = () => {
   const inputAmount = document.getElementById("depositamount");
   document.getElementById("mydeposit").addEventListener("click", (event) => {
   inputElement.value = parseInt(inputElement.value) +1;
-
-  })
- }
+  });
+ };
 
 
 const withdraw = () => {
   const inputAmount = document.getElementById("withdrawalamount");
   document.getElementById("mywithdrawal").addEventListener("click", (event) => {
   inputElement.value = parseInt(inputElement.value) -1;
-  })
- }
+  });
+ };
 
 const increase = () => {
   const inputElement = document.getElementById("consumption_daily_actual_consumption");
