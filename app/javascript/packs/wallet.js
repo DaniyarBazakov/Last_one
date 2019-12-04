e99943b174fbbc8958ec513bb30daaa1ae8a7031
bnadal <benjamin-nadal@hotmail.fr>
2019-12-04 11:21:21 -0500

    fixed syntax

--- app/javascript/packs/wallet.js
+++ app/javascript/packs/wallet.js
@@ -4,14 +4,16 @@ const deposit = () => {
   document.getElementById("mydeposit").addEventListener("click", (event) => {
   inputElement.value = parseInt(inputElement.value) +1;
 
-}
+  })
+ }
 
 
 const withdraw = () => {
   const inputAmount = document.getElementById("withdrawalamount");
   document.getElementById("mywithdrawal").addEventListener("click", (event) => {
   inputElement.value = parseInt(inputElement.value) -1;
-}
+  })
+ }
 
 const increase = () => {
   const inputElement = document.getElementById("consumption_daily_actual_consumption");
