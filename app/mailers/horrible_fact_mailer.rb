e02c2b20be57aa5e144919fc9c9a76d5f1ae57dd
Daniyar Bazakov <bazakovd@gmail.com>
2019-12-06 09:33:49 -0500

    mail

--- app/mailers/horrible_fact_mailer.rb
+++ app/mailers/horrible_fact_mailer.rb
@@ -14,6 +14,16 @@ class HorribleFactMailer < ApplicationMailer
     # This will render a view in `app/views/horrible_fact_mailer`!
   end
 
+  def scare_again_user
+    @user = params[:user] # Instance variable => available in view
+    @horrible_fact = HorribleFact.all.sample(1)[0]
+    @horriblefactphoto = @horrible_fact.photo
+    @horriblefacttext = @horrible_fact.fact
+    mail(to: @user.email, subject: 'Stop smoking !!!')
+    # mail(to: 'jovialiste82@yahoo.fr', subject: 'Stop smoking !!!')
+    # This will render a view in `app/views/horrible_fact_mailer`!
+  end
+
    # def scareuser
    #    user = User.all.sample(1)[0]  VS  user = User.first
    #    # This is how you pass value to params[:user] inside mailer definition!
